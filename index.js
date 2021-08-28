d06174f0198fba63e994e6d95be8c914914f67d2
Mike Veneziale <falciformdesign@gmail.com>
2021-08-28 11:32:39 -0500

    got string interpolation for user input working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateHTML = require('./src/page-template');
 
 // variable that holds array of questions for inquirer
 const questions = [
@@ -18,8 +19,24 @@ const promptUser = () => {
   .prompt(questions)
 }
 
+const writeToFile = function(fileName, response) {
+  fs.writeFile(fileName, response, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("success");
+  })
+} 
+
+
 
 promptUser()
 .then(function(managerAnswers) {
   return generateHTML(managerAnswers);
-});
+})
+.then(function(pageHTML) {
+  return writeToFile('index.html', pageHTML);
+})
+// .then(pageHTML => {
+//   return writeFile(pageHTML);
+// });
