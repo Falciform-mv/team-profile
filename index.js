6fe67ad78f85f77cfc99c295a0bdc89494f1235d
Mike Veneziale <falciformdesign@gmail.com>
2021-08-28 10:56:55 -0500

    new files

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 
 // Display page that has a software teams emails and github profiles
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// variable that holds array of questions for inquirer
+const questions = [
+  {
+    type: 'input',
+    name: 'manager',
+    message: 'Who is your team manager?'
+  }
+]
+
+const promptUser = () => {
+  return inquirer
+  .prompt(questions)
+}
+
+
+promptUser()
+.then(function(managerAnswers) {
+  return generateHTML(managerAnswers);
+});
